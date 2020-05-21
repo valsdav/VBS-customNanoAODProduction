feb7d49f6f2db507a3c36cee82d0e3f7683aa813
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2020-05-21 08:23:20 -0500

    submit 2018 mc

--- NanoAODProduction/test/submit.py
+++ NanoAODProduction/test/submit.py
@@ -1,11 +1,11 @@
 from CRABClient.UserUtilities import config, getUsernameFromCRIC
 
-samples_list = 'samples2017.txt'
-#samples_list = 'samples2018.txt'
-pset_file = 'nano_production_mc_2017_NANO.py'
-#pset_file = 'nano_production_mc_2018_NANO.py'
-out_dataset_tag = 'VVjj_2017v6'
-#out_dataset_tag = 'VVjj_2018v6'
+#samples_list = 'samples2017.txt'
+samples_list = 'samples2018.txt'
+#pset_file = 'nano_production_mc_2017_NANO.py'
+pset_file = 'nano_production_mc_2018_NANO.py'
+#out_dataset_tag = 'VVjj_2017v6'
+out_dataset_tag = 'VVjj_2018v6'
 storage_location = '/store/group/lnujj/VVjj_aQGC/custom_nanoAOD/'
 
 config = config()
@@ -21,7 +21,7 @@ config.JobType.allowUndistributedCMSSW = True
 
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 5
+config.Data.unitsPerJob = 1
 
 config.Data.publication = True
 config.Data.outputDatasetTag = out_dataset_tag
